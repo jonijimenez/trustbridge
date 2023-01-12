e5c5306962d1c5e5555a1b66d9cdc6abefcc8327
joni <joni.jimenez@parcelpoint.com.au>
2023-01-12 15:36:23 +0800

    Refactor files to specific files

--- src/main/java/com/trustbridge/demo/TouristNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.trustbridge.demo;
-
-public class TouristNotFoundException extends RuntimeException {
-    TouristNotFoundException(Long id) {
-        super("Could not find tourist" + id);
-    }
-}
